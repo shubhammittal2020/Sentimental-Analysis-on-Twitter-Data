75c8b0c3c2d40ecbac717309ec733fe440378b59
shubhammittal2020 <shubhammittal2020@gmail.com>
2018-08-01 00:37:46 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key='v3yezHywrApvm1hxKL493jDRL'
-consumer_secret='reGAy6P4F3erzhOoNXI6BCc2hn5DBlyjuLeOVFYFfjuEyREujI'
+consumer_key='_____________'
+consumer_secret='__________'
 
-access_token='981943034928562177-yYI8alatLNapKMwBXDSFGPEEEBK39cQ'
-access_token_secret='UMiAJMz4NHSmbEXgWXSxUEwC53objnJ7of74sdAl484da'
+access_token='_________________'
+access_token_secret='__________'
 
 auth=tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
